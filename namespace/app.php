4829546fbfc1ad43c63b398c4283793cab4d9a82
rodolfobandeira <contato@rodolfobandeira.com>
2015-05-30 14:19:12 -0400

    Stuck on namespace

--- namespace/app.php
+++ namespace/app.php
@@ -1,9 +1,14 @@
 <?php
-namespace Veiculo;
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-use Veiculo\Carro;
+use Veiculo\Carro\Car;
+
+function my_autoloader($class) {
+    include 'Veiculo/' .$class . '.php';
+}
+spl_autoload_register('my_autoloader');
+
 
 $carro = new Car();
 $carro->honk();
-
-
