1bd3aed6441c465ca89f472dcc90cad5b85f6ad1
rodolfobandeira <contato@rodolfobandeira.com>
2015-06-30 07:40:25 -0400

    tricks

---
+++ strings/printf-2.php
@@ -0,0 +1,18 @@
+<?php
+echo PHP_EOL;
+printf('%08f', 1.01);
+// 1.010000
+// 
+echo PHP_EOL;
+printf('%-06b', 15);
+// 111100
+//
+echo PHP_EOL;
+printf('%06b', 15);
+// 001111
+echo PHP_EOL;
+
+
+echo PHP_EOL;
+printf('%-08f', 1.01);
+// 1.010000
