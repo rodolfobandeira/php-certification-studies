2e17254267690bdf7b6ba055e489a7e4943d9b6a
rodolfobandeira <contato@rodolfobandeira.com>
2015-07-26 21:39:10 -0400

    strtotime + func_num_args

---
+++ datetime-management/strtotime-est-cst.php
@@ -0,0 +1,9 @@
+<?php
+
+$a = strtotime ('00:00:00 Feb 23 1976 EST');
+
+$b = strtotime ('00:00:00 Feb 23 1976 CST');
+
+echo $a - $b;
+
+
---
+++ functions/func_num_args.php
@@ -0,0 +1,16 @@
+<?php
+
+function addValues() {
+    $sum = 0;
+
+    for ($i = 1; $i <= func_num_args(); $i++){
+echo $i. PHP_EOL;
+
+        $sum += func_get_arg($i);
+    }
+
+    return $sum;
+}
+
+echo addValues(1, 2, 3);
+
