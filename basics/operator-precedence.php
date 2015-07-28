e341806ece9ffd989f1edb3822956f39551967d3
rodolfobandeira <contato@rodolfobandeira.com>
2015-07-27 21:10:50 -0400

    operator precedence

---
+++ basics/operator-precedence.php
@@ -0,0 +1,17 @@
+<?php
+
+$o_que_vem_antes__multiplicacao_ou_divisao = 6 / 2 * 2;
+
+echo '6 / 2 * 2 = ';
+echo $o_que_vem_antes__multiplicacao_ou_divisao. PHP_EOL;
+
+
+$o_que_vem_antes__multiplicacao_ou_divisao = 6 * 2 / 2;
+
+echo '6 * 2 / 2 = ';
+echo $o_que_vem_antes__multiplicacao_ou_divisao. PHP_EOL;
+
+/*
+ * Mesma coisa! Same thing!
+ * If you choose / or * doesn't matter!
+ */
