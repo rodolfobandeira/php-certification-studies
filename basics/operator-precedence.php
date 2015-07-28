c2537ebb783d6111f8184739f113a780e7702a9f
rodolfobandeira <contato@rodolfobandeira.com>
2015-07-27 21:18:49 -0400

    Fixing matematical operator precedence

--- basics/operator-precedence.php
+++ basics/operator-precedence.php
@@ -1,17 +1,23 @@
 <?php
 
-$o_que_vem_antes__multiplicacao_ou_divisao = 6 / 2 * 2;
+$o_que_vem_antes__multiplicacao_ou_divisao = 2 / 2 * 10;
 
-echo '6 / 2 * 2 = ';
+echo '2 / 2 * 10 = ';
 echo $o_que_vem_antes__multiplicacao_ou_divisao. PHP_EOL;
 
 
-$o_que_vem_antes__multiplicacao_ou_divisao = 6 * 2 / 2;
+$o_que_vem_antes__multiplicacao_ou_divisao = 2 * 2 / 10;
 
-echo '6 * 2 / 2 = ';
+echo '2 * 2 / 10 = ';
 echo $o_que_vem_antes__multiplicacao_ou_divisao. PHP_EOL;
 
 /*
- * Mesma coisa! Same thing!
- * If you choose / or * doesn't matter!
+ * When we're trying to compare multiplication and division
+ * that one that comes first from left to right is executed
+ * first.
+ *
+ * output:
+ * 2 / 2 * 10 = 10
+ * 2 * 2 / 10 = 0.4
+ *
  */
