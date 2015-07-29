cb3484773879ad5bd9ede293c7d161a101402388
rodolfobandeira <contato@rodolfobandeira.com>
2015-07-28 23:30:08 -0400

    Regex: \w\W\s\S\w+\s+

---
+++ regex/w-s-W-S.php
@@ -0,0 +1,16 @@
+<?php
+/*
+        \s match any white space character [\r\n\t\f ]
+        \S match any non-white space character [^\r\n\t\f ]
+        \w match any word character [a-zA-Z0-9_]
+        \W match any non-word character [^a-zA-Z0-9_]
+        \s+ match any white space character [\r\n\t\f ]
+            Quantifier: + Between one and unlimited times, as many times as possible, giving back as needed [greedy]
+        \S+ match any non-white space character [^\r\n\t\f ]
+            Quantifier: + Between one and unlimited times, as many times as possible, giving back as needed [greedy]
+        \w+ match any word character [a-zA-Z0-9_]
+            Quantifier: + Between one and unlimited times, as many times as possible, giving back as needed [greedy]
+        \W+ match any non-word character [^a-zA-Z0-9_]
+            Quantifier: + Between one and unlimited times, as many times as possible, giving back as needed [greedy]
+ */
+
